3df594f8cbe9bc702ea4661bf23fe5d18b605eec
KilJae69 <adi.toromanovic@outlook.com>
2024-06-19 14:31:45 +0200

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 import React from "react";
 
 import MobileNavbar from "@/components/mobile-navbar";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 // const inter = Inter({ subsets: ["latin"] });
 
