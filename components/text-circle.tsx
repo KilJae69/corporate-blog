95eea99fa47a62d2a0bbbdfc49176dd072fbb742
KilJae69 <adi.toromanovic@outlook.com>
2024-06-27 11:18:41 +0200

    Fixed scrolling behaviour for pages

--- components/text-circle.tsx
+++ components/text-circle.tsx
@@ -1,19 +1,27 @@
-
+"use client"
 import Link from "next/link";
+
 import { FaArrowDown } from "react-icons/fa6";
 
 export default function TextCircle() {
+  const handleScroll = (event:any) => {
+    event.preventDefault();
+    const target = document.querySelector("#what-we-do");
+   if(target)  target.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <Link
       href={"#what-we-do"}
       aria-label="Scroll down to the next section"
       className="relative block w-max hover:cursor-pointer"
+      onClick={handleScroll}
     >
       <svg
         className="relative size-[150px] animate-spin-slow  md:size-[200px] lg:size-[250px]"
         viewBox="0 0 250 250"
       >
         <path
+        onClick={e => e.preventDefault()}
           id="curve"
           d="M 125, 125 m -100, 0 a 100,100 0 1,1 200,0 a 100,100 0 1,1 -200,0"
         ></path>
